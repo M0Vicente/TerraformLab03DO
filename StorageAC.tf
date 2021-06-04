c361fd888fab4860da708e510624b8a8da20d93f
M0Vicente <marcioovicente@yahoo.com.br>
2021-06-04 13:24:25 -0300

    Lab DevOps commit ##3

--- StorageAC.tf
+++ StorageAC.tf
@@ -10,15 +10,15 @@ resource "azurerm_storage_account" "mystorageaccount" {
         environment = "Terraform Demo"
     }
 }
-### CREATE STORAGE ACCOUNT --> IMPORT STATE TF.
-resource "azurerm_storage_account" "mystorageaccountlg01" {
-    name                        = "mystoglgd01"
-    resource_group_name         = azurerm_resource_group.myterraformgroup.name
-    location                    = "eastus"
-    account_replication_type    = "LRS"
-    account_tier                = "Standard"
-
-    tags = {
-        environment = "Terraform Demo"
-    }
-}
+#### CREATE STORAGE ACCOUNT --> IMPORT STATE TF.
+#resource "azurerm_storage_account" "mystorageaccountlg01" {
+#    name                        = "mystoglgd01"
+#    resource_group_name         = azurerm_resource_group.myterraformgroup.name
+#    location                    = "eastus"
+#    account_replication_type    = "LRS"
+#    account_tier                = "Standard"
+#
+#    tags = {
+#        environment = "Terraform Demo"
+#    }
+#}
